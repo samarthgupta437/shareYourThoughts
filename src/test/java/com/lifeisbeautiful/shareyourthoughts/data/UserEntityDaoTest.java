ee1c8275d6d67f55e7ad96216456cbffb4e059d2
Samarth Gupta <samarth.gupta@appdynamics.com>
2018-03-14 10:32:51 +0530

    dummy test

---
+++ src/test/java/com/lifeisbeautiful/shareyourthoughts/data/UserEntityDaoTest.java
@@ -0,0 +1,12 @@
+package com.lifeisbeautiful.shareyourthoughts.data;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserEntityDaoTest {
+	
+	@Test
+	public void testUserEntity() {
+		Assert.assertTrue(true);
+	}
+}

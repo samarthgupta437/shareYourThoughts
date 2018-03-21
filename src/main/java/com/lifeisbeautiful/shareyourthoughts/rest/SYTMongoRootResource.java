93a8ca70e5dcde380d436ee03c3f2d998ef817e3
Samarth Gupta <samarth.gupta@appdynamics.com>
2018-03-21 11:38:18 +0530

    integrate with rest

--- src/main/java/com/lifeisbeautiful/shareyourthoughts/rest/SYTMongoRootResource.java
+++ src/main/java/com/lifeisbeautiful/shareyourthoughts/rest/SYTMongoRootResource.java
@@ -2,12 +2,17 @@ package com.lifeisbeautiful.shareyourthoughts.rest;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -31,5 +36,17 @@ public class SYTMongoRootResource {
 		} catch (Exception e) {
 			throw new SytException();
 		} 
+	}
+	
+	@GET
+	@Path("/posts")
+	public Response findPost( @QueryParam("key") @DefaultValue("") String key) {
+		try {
+			List<Post> matchedPosts = new ArrayList<Post>();
+			return Response.ok().entity(matchedPosts).build();
+		} catch (Exception e) {
+			throw new SytException();
+		}
+
 	}
 }
